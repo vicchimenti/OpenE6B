6d7d4ee02442e3497568c96a23589409eb0d6996
Karthikeyan Reddy Rokkam <rokkamkarthi@seattleu.edu>
2019-05-30 18:33:13 -0700

    run stress suite, bkg processes

--- OpenE6B/OpenE6B/Scripts/StressTestApp.ps1
+++ OpenE6B/OpenE6B/Scripts/StressTestApp.ps1
@@ -1,6 +1,6 @@
 ﻿param (
-	#[parameter(Mandatory=$true)]
-	#[String] $TestDllPath,
+	[parameter(mandatory=$true)]
+	[string] $TestDllPath,
 
 	[parameter(Mandatory=$true)]
 	[int] $NumBackGroundProcesses,
@@ -11,11 +11,17 @@
 
 #$slnPath = "C:\Users\rkkr9\Desktop\MSCS_SQ19\Testing n Debugging\projects\OpenE6B\OpenE6B\OpenE6B.sln";
 
-$TestDllPath = "C:\Users\rkkr9\Desktop\MSCS_SQ19\Testing n Debugging\projects\OpenE6B\OpenE6B\OpenE6BTests\bin\Debug\OpenE6BTests.dll";
+#$TestDllPath = "C:\Users\rkkr9\Desktop\MSCS_SQ19\Testing n Debugging\projects\OpenE6B\OpenE6B\OpenE6BTests\bin\Debug\OpenE6BTests.dll";
 $runTestSuite = ".\RunTestSuite.ps1"
 
 Write-Host "Stress Tests started at $(Get-Date)" -ForegroundColor Green;
 Write-Host;
-& "$($runTestSuite)" "$($TestDllPath)" "$($NumberOfTimes)"
+
+for($i = 0; $i -lt $NumBackGroundProcesses; $i++) {
+	Start-Job -ScriptBlock {
+		& "$($runTestSuite)" "$($TestDllPath)" "$($NumberOfTimes)";
+	}
+}
+
 Write-Host;
 Write-Host "Stress Tests ended at $(Get-Date)" -ForegroundColor Green;
